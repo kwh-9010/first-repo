238c0d8e4d5caad08fb942310d8e57f76a9a31f9
kwh9010 <kwh9010@gmail.com>
2020-04-27 14:52:20 +0900

    test git

---
+++ practice.py
@@ -0,0 +1,13 @@
+# import sys
+# print("python", "Java", file=sys.stdout)
+# print("python", "Java", file=sys.stderr)
+
+# scores = {"수학" : 0, "영어" : 50, "코딩" : 100}
+# for subject, scores in scores.items():
+#     print(subject.ljust(8), str(scores).rjust(4), sep=":")
+
+# for num in range(1, 21):
+#     print("대기번호 : " + str(num).zfill(3))
+
+answer = input("아무값이나 입력하세요 : ")
+print("입력하신 값은" + answer + "입니다.")
